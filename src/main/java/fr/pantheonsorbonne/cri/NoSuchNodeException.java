cdffc40011c5011db4a080ee674315333e91e27f
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2025-11-02 15:48:39 +0100

    updated with docker image creation from maven

--- src/main/java/fr/pantheonsorbonne/cri/NoSuchNodeException.java
+++
@@ -1,5 +0,0 @@
-package fr.pantheonsorbonne.cri;
-
-public class NoSuchNodeException extends Exception{
-
-}
--- src/main/java/fr/pantheonsorbonne/cri/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package is the main package of this template.
- */
-package fr.pantheonsorbonne.cri;
-
